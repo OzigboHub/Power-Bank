c70db358e38d591df1543379844b8fe4e99e844a
emmanuel <eoziegbe2@gmail.com>
2023-05-22 13:07:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+
+//card 1
+function myFunction1() {
+    var dropdownContent = document.getElementById("myDropdown1");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+}
+
+function clicked1(){
+    document.getElementById("likebtn1").style.color = "red"
+}
+
+//card 2
+
+function myFunction2() {
+    var dropdownContent = document.getElementById("myDropdown2");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+}
+
+function clicked2(){
+    document.getElementById("likebtn2").style.color = "red"
+}
+
+//card 3
+
+function myFunction3() {
+    var dropdownContent = document.getElementById("myDropdown3");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+}
+
+function clicked3(){
+    document.getElementById("likebtn3").style.color = "red"
+}
+
+//card 4
+
+function myFunction4() {
+    var dropdownContent = document.getElementById("myDropdown4");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+}
+
+function clicked4(){
+    document.getElementById("likebtn4").style.color = "red"
+}
+
+//card 5
+
+function myFunction5() {
+    var dropdownContent = document.getElementById("myDropdown5");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+}
+
+function clicked5(){
+    document.getElementById("likebtn5").style.color = "red"
+}
+
+//card 6
+
+function myFunction6() {
+    var dropdownContent = document.getElementById("myDropdown6");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+}
+
+function clicked6(){
+    document.getElementById("likebtn6").style.color = "red"
+}
+
+
+
+
+
